c72206c4797e3ac89d49afba500a353cdd1827c9
Tom Scott <tscott@telvue.com>
2014-02-21 02:24:12 -0500

    Documentation

--- lib/bowery/assetfile.rb
+++ lib/bowery/assetfile.rb
@@ -1,22 +1,30 @@
 require 'bowery/components'
 require 'bowery/component'
 
-# Helper methods for the Assetfile.
+# The Assetfile DSL. These methods are mixed in and used by the
+# Assetfile to build the `components` collection used to actually
+# install Bower assets. These methods create Component classes, and are
+# shoveled into the big collection.
 
 module Bowery
   module Assetfile
+    # Specify an alternative API other than the official Bower registry.
     def source api
       return if api == :bower
     end
 
+    # Specify a javascript component to install.
     def js name, version='', options={}
       component name, version, options.merge(js: true)
     end
 
+    # Specify a stylesheet component to install.
     def css name, version='', options={}
       component name, version, options.merge(css: true)
     end
 
+    # Specify an open-ended component to install. Useful for components
+    # with both JS and CSS assets, or neither.
     def component name, version='', options={}
       attributes = options.merge name: name, version: version
       components << Component.new(attributes)
