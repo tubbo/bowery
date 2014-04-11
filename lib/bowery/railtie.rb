89082c7894cd5ed0b82564b7b94da0403f0e37a3
Tom Scott <tscott@telvue.com>
2014-04-11 15:31:26 -0400

    Fix railtie and update dummy app to rails 4.1

--- lib/bowery/railtie.rb
+++ lib/bowery/railtie.rb
@@ -1,11 +1,14 @@
-require 'rails'
+require 'rails/railtie'
 
 module Bowery
+  # When integrated with a Rails project, this configures Sprockets so
+  # that Bower components are available within the asset pipeline
+  # automatically.
   class Railtie < Rails::Railtie
-    config.bowery = ActiveSupport::OrderedOptions.new
-
-    rake_tasks do
-      load 'lib/tasks/bowery.rake'
+    initializer "add bower components path to sprockets" do
+      if config.assets.present?
+        config.assets.paths << 'vendor/components'
+      end
     end
   end
 end
